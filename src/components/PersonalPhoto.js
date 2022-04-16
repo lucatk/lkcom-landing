eb5297c0c174234c352b5f41dabd26a9305bb8bf
lucatk <mail@lucakillmaier.com>
2022-04-16 17:47:12 +0200

    Update gatsby deps, change profile photo

--- src/components/PersonalPhoto.js
+++ src/components/PersonalPhoto.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import { GatsbyImage } from "gatsby-plugin-image";
+
 
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      photo: file(relativePath: { eq: "photo_muc.jpg" }) {
+      photo: file(relativePath: { eq: "IMG_9476-Edit_square_hq.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 500, maxHeight: 500, quality: 90, cropFocus: CENTER) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
+          gatsbyImageData(
+            aspectRatio: 1,
+            quality: 90,
+            placeholder: BLURRED
+          )
         }
       }
     }
   `)
 
-  if (!data?.photo?.childImageSharp?.fluid) {
-    return <div>Picture not found</div>
+  if (!data?.photo?.childImageSharp?.gatsbyImageData) {
+    return null
   }
 
-  return <Img fluid={data.photo.childImageSharp.fluid} />
+  return <GatsbyImage image={data.photo.childImageSharp.gatsbyImageData} />
 }
 
 export default Image
